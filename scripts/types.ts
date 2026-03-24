f98240ca910f41e5d65663bafea774c435fe89d9
Diogo Martino <diogo.martino10@gmail.com>
2026-03-24 21:48:00 +0000

    wip: testing

--- scripts/types.ts
+++ scripts/types.ts
@@ -1,11 +1,18 @@
 import z from 'zod';
 
+const zPluginId = z
+  .string()
+  .min(1, 'Plugin ID is required')
+  .regex(
+    /^[a-z0-9-]+$/,
+    'Plugin ID must contain only lowercase letters, numbers, and dashes'
+  );
+
 const zPlugin = z.object({
-  id: z.string(),
+  id: zPluginId,
   name: z.string(),
   description: z.string(),
   author: z.string(),
-  repo: z.string(),
   logo: z.url(),
   homepage: z.string().optional(),
   tags: z.array(z.string()).optional(),
@@ -16,12 +23,18 @@ const zPluginVersion = z.object({
   version: z.string(),
   downloadUrl: z.url(),
   checksum: z.string(),
-  sdkRange: z.string(),
+  sdkVersion: z.number().int().nonnegative(),
   size: z.number()
 });
 
 type TPlugin = z.infer<typeof zPlugin>;
 type TPluginVersion = z.infer<typeof zPluginVersion>;
+type TPluginsIndex = {
+  plugin: TPlugin & {
+    verified: boolean;
+  };
+  versions: TPluginVersion[];
+};
 
 export { zPlugin, zPluginVersion };
-export type { TPlugin, TPluginVersion };
+export type { TPlugin, TPluginVersion, TPluginsIndex };
